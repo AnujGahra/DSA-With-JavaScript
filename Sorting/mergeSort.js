713970048ef6ccfbb6cee36b0c2f46c79836c8f0
AnujGahra <anujgahra244304@gmail.com>
2025-02-25 08:01:32 +0530

    mergeSort

--- Sorting/mergeSort.js
+++ Sorting/mergeSort.js
@@ -1,39 +1,40 @@
 const mergeSort = (arr) => {
-    if (arr.length < 2) {
-        return arr;
-    }
-    let mid = Math.floor(arr.length / 2);
-    let left = mergeSort(arr.slice(0, mid))
-    let right = mergeSort(arr.slice(mid))
-    return merge(left, right)
-}
+  if (arr.length < 2) {
+    return arr;
+  }
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+};
 
 const merge = (left, right) => {
-    const result = []
-    let leftIndex = 0, rightIndex = 0;
-    while (leftIndex < left.length && rightIndex < right.length) {
-        if (left[leftIndex] < right[rightIndex]) {
-            result.push(left[leftIndex])
-            leftIndex++;
-        }
-        else {
-            result.push(right[rightIndex])
-            rightIndex++;
-        }
+  const result = [];
+  let leftIndex = 0,
+    rightIndex = 0;
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      result.push(right[rightIndex]);
+      rightIndex++;
     }
+  }
 
-    while (leftIndex < left.length) {
-        result.push(left[leftIndex])
-        leftIndex++;
-    }
+  while (leftIndex < left.length) {
+    result.push(left[leftIndex]);
+    leftIndex++;
+  }
 
-    while (rightIndex < right.length) {
-        result.push(right[rightIndex])
-        rightIndex++;
-    }
+  while (rightIndex < right.length) {
+    result.push(right[rightIndex]);
+    rightIndex++;
+  }
 
-    return result;
-}
+  return result;
+};
 
-const arr1 = [29, 10, 8, 16, 37, 14, 4, 45]
-console.log(mergeSort(arr1))
+const arr1 = [29, 10, 8, 16, 37, 14, 4, 45];
+console.log(mergeSort(arr1));
+
